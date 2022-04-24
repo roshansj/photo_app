4e67771d44265c667e5007c700cf4ad757773e65
gachempa <jpsg514@gmail.com>
2022-04-23 18:38:11 -0700

    added openbbrowser py file

---
+++ OpenBrowser.py
@@ -0,0 +1,8 @@
+# import the webbrowser module
+import webbrowser
+  
+# select the desired url
+url = "https://www.bishopoconnell.org/"
+  
+# call the default open url method
+webbrowser.open(url)
